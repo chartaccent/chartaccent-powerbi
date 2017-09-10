d45e08570c429a42b7acda53a82389544a4651cc
Donghao Ren <donghao.ren@gmail.com>
2017-09-09 23:10:00 -0700

    initial commit

---
+++ make.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Bar chart..."
+rm -rf .tmp
+cp pbiviz.barchart.json pbiviz.json
+pbiviz update
+pbiviz package
+
+echo "Line chart..."
+rm -rf .tmp
+cp pbiviz.linechart.json pbiviz.json
+pbiviz package
+
+echo "Scatterplot..."
+rm -rf .tmp
+cp pbiviz.scatterplot.json pbiviz.json
+pbiviz package
